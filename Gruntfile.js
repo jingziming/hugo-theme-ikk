e1e0e0089de1a5709ddbcd70c5626435d4e95c51
jingziming <me@jingziming.com>
2017-07-14 15:43:55 +0800

    Add development tool

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+
+module.exports = function(grunt) {
+    'use strict';
+    require('load-grunt-tasks')(grunt, {
+        pattern: ['grunt-*']
+    });
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        config: {
+            'cssSrcDir': 'src/sass',
+            'cssTargetDir': 'static/css'
+        },
+        sass: {
+            build: {
+                options: {
+                    sourceMaps: false,
+                    outputStyle: 'compressed',
+                },
+                files: {
+                    '<%=  config.cssTargetDir %>/styles.css': '<%=  config.cssSrcDir %>/**/*.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '<%=  config.cssSrcDir %>/**/*.scss',
+                tasks: ['sass:build']
+            }
+        }
+    });
+    grunt.registerTask('default', [
+        'sass:build',
+        'watch'
+    ]);
+};
